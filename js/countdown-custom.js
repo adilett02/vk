6d5364155b6f8268a1f9e0d3d64d00eb99f290aa
Adilet <adiletturdakunov2002@gmail.com>
2020-11-28 15:00:44 +0600

    first commit

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2020, 12-1, 7)}); // year, month, date, hour
+        });
+});		
+
